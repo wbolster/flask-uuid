880ef557090ae65530d73a19f28ee2b707d0872d
Wouter Bolsterlee <uws@xs4all.nl>
2013-03-15 17:40:01 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Flask-UUID',
+    version='0.1',
+    description='UUID url converter for Flask routes',
+    author='Wouter Bolsterlee',
+    author_email='uws@xs4all.nl',
+    url='https://github.com/wbolster/flask-uuid',
+    license='BSD',
+    py_modules=['flask_uuid'],
+)
