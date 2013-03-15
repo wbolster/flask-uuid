9c43e994500484a6c4b7ecdbefe68cd20a84ba90
Wouter Bolsterlee <uws@xs4all.nl>
2013-03-15 21:52:05 +0100

    Add example file

---
+++ example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import uuid
+
+from flask import Flask, redirect, url_for
+from flask.ext.uuid import FlaskUUID
+
+app = Flask(__name__)
+FlaskUUID(app)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('example1', id=uuid.uuid4()))
+
+
+@app.route('/example1/<uuid:id>/')
+def example1(id):
+    return repr(id)
+
+
+@app.route('/example2/<uuid(strict=False):id>/')
+def example2(id):
+    return repr(id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
